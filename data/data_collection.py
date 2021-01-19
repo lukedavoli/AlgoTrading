2e00ad2929209f7394ad78b0c75a8a2775b41346
lukedavoli <ldavoli.mail@gmail.com>
2021-01-19 18:01:37 +1100

    Formatting

--- data/data_collection.py
+++ data/data_collection.py
@@ -1,17 +1,16 @@
-import json
-import yaml
 import sqlite3
 import time
 from candle import Candle
 from api import Bittrex
 from dao import Dao
 
-MARKETS = ['BTC-USDT', 'ETH-USDT', 'ADA-USDT', 'XLM-USDT', 'LINK-USDT', 'DOT-USDT', 'LTC-USDT', 'NEO-USDT', 'TRX-USDT', 
-    'BSV-USDT', 'BCH-USDT', 'ENJ-USDT', 'LBC-USDT', 'BAT-USDT', 'ETC-USDT', 'DGB-USDT', 'DOGE-USDT', 'EOS-USDT', 
-    'XTZ-USDT', 'ALGO-USDT', 'ATOM-USDT', 'RVN-USDT']
+MARKETS = [
+	'BTC-USDT', 'ETH-USDT', 'ADA-USDT', 'XLM-USDT', 'LINK-USDT', 'DOT-USDT', 'LTC-USDT', 'NEO-USDT',
+	'TRX-USDT', 'BSV-USDT', 'BCH-USDT', 'ENJ-USDT', 'LBC-USDT', 'BAT-USDT', 'ETC-USDT', 'DGB-USDT',
+	'DOGE-USDT', 'EOS-USDT', 'XTZ-USDT', 'ALGO-USDT', 'ATOM-USDT', 'RVN-USDT'
+]
 
 if __name__ == '__main__':
-
 	bittrex = Bittrex()
 	db = Dao()
 
@@ -20,9 +19,12 @@ if __name__ == '__main__':
 	except sqlite3.OperationalError:
 		pass
 
+
 	for market in MARKETS:
-		time.sleep(2)
-		candles = bittrex.get_candles(symbol=market, candle_interval='MINUTE_5', candle_type='TRADE')
+		time.sleep(1)
+		candles = bittrex.get_candles(
+			symbol=market, candle_interval='MINUTE_5', candle_type='TRADE'
+		)
 		for candle in candles:
 			next_candle = Candle(market=market, btrx_dict=candle)
 			db.add_candle(next_candle)
